2cce6615d74a8d99a15e65f671e55c7a7de88d82
AT Wolfar <atwolfar@macbook-air-at.local>
2019-03-18 20:53:44 +0100

    Initial Commit

---
+++ PsychoTestsARTests/PsychoTestsARTests.swift
@@ -0,0 +1,34 @@
+//
+//  PsychoTestsARTests.swift
+//  PsychoTestsARTests
+//
+//  Created by AT Wolfar on 18/03/2019.
+//  Copyright © 2019 PUMTeam. All rights reserved.
+//
+
+import XCTest
+@testable import PsychoTestsAR
+
+class PsychoTestsARTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
